b743f5fd35237da07b16dcc96dd2173ec553d006
akash8949 <96293780+akash8949@users.noreply.github.com>
2022-02-14 17:25:43 +0530

    chang1

--- src/A1.java
+++ src/A1.java
@@ -3,6 +3,7 @@ public class A1 {
 	public static void main(String[] args) {
 		System.out.println("Welcome Akash On GitHub");
 		//ass
+		System.out.println("Welcome akash back to eclips");
 	}
 
 }
